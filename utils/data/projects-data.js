6e821de8e975df68f9d5c93d1ec96d1eee3284bf
Cliff263 <cliffjaure263@gmail.com>
2024-09-29 11:00:44 +0200

    Feature Update

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,51 +1,65 @@
-import devopsAmplify from '/public/image/amplify.png';  // Placeholder image for CI/CD to AWS Amplify
-import devopsLambda from '/public/image/lambda.png';  // Placeholder image for CI/CD to Lambda and S3
-import laravelProject from '/public/image/appraisal.png';  // Placeholder image for Laravel project
-import awsEks from '/public/image/aws-eks.png';  // Placeholder image for AWS EKS and Terraform project
+import ayla from '/public/image/ayla.jpg';
+import crefin from '/public/image/crefin.jpg';
+import realEstate from '/public/image/real-estate.jpg';
+import travel from '/public/image/travel.jpg';
 
 export const projectsData = [
     {
         id: 1,
-        name: 'CI/CD Pipeline - Deploying to AWS Amplify',
-        description: 'I configured a CI/CD pipeline to automate the deployment of a web application to AWS Amplify. This involved setting up GitHub Actions to trigger automatic builds and deployments on every code push. AWS Amplify was used for hosting the front-end, and the entire process was streamlined with version control and continuous integration.',
-        tools: ['GitHub Actions', 'AWS Amplify', 'GitHub', 'AWS S3', 'AWS CloudFront'],
-        role: 'DevOps Engineer',
+        name: 'AI Powered Financial App',
+        description: "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
+        tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
+        role: 'Backend Developer',
         code: '',
         demo: '',
-        image: devopsAmplify,
+        image: crefin,
     },
     {
         id: 2,
-        name: 'CI/CD Pipeline with CloudFormation - Deploying Lambda and S3',
-        description: 'I developed a CI/CD pipeline using AWS CloudFormation and GitHub Actions to automate the deployment of a serverless application. This included deploying AWS Lambda functions and uploading static files to an S3 bucket. I also used CloudFormation templates to provision and manage the infrastructure as code, ensuring efficient and scalable deployments.',
-        tools: ['AWS CloudFormation', 'AWS Lambda', 'AWS S3', 'GitHub Actions', 'CloudWatch', 'API Gateway'],
-        role: 'DevOps Engineer',
-        code: '',
-        demo: '',
-        image: devopsLambda,
-    },
-    {
-        id: 3,
-        name: 'Performance Appraisal System - PHP Laravel and Tailwind CSS',
-        description: 'I built a performance appraisal system using PHP Laravel for the backend and Tailwind CSS for the frontend. The system allows HR teams to evaluate employee performance, generate reports, and manage feedback. Tailwind CSS ensured a modern and responsive UI, while Laravel handled business logic, database management, and security.',
-        tools: ['PHP', 'Laravel', 'Tailwind CSS', 'MySQL', 'Docker', 'Nginx'],
+        name: 'Travel Agency App',
+        description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
+        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
         role: 'Full Stack Developer',
         code: '',
         demo: '',
-        image: laravelProject,
+        image: travel,
+    },
+    {
+        id: 3,
+        name: 'AI Powered Real Estate',
+        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
+        tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
+        code: '',
+        role: 'Full Stack Developer',
+        demo: '',
+        image: realEstate,
     },
     {
         id: 4,
-        name: 'AWS EKS Cluster Setup with Terraform',
-        description: 'I provisioned a complete AWS infrastructure using Terraform, including setting up an AWS EKS cluster. This project involved deploying and managing Kubernetes clusters on AWS using EKS, configuring VPC, subnets, security groups, and other networking components. Terraform was used for infrastructure-as-code, making the setup highly reproducible and scalable.',
-        tools: ['AWS EKS', 'Terraform', 'AWS VPC', 'ELB', 'EC2', 'IAM', 'Kubernetes'],
-        role: 'DevOps Engineer',
+        name: 'Newsroom Management',
+        description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
+        tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
         code: '',
         demo: '',
-        image: awsEks,
+        image: ayla,
+        role: 'Full Stack Developer',
     }
 ];
 
+
+// Do not remove any property.
+// Leave it blank instead as shown below
+
+// {
+//     id: 1,
+//     name: '',
+//     description: "",
+//     tools: [],
+//     role: '',
+//     code: '',
+//     demo: '',
+//     image: crefin,
+// },
 
 
 // Do not remove any property.
